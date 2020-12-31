2337f1107cbdf1c8005a2384e3bdc97f9d7298fe
Senghoung469 <senghoung469@gmail.com>
2020-12-31 16:42:42 +0700

    design_home1

--- dc-front/src/layouts/Header.js
+++ dc-front/src/layouts/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import ServiceApi from '../Service';
-import ScrollUpButton from "react-scroll-up-button"; //Add this line Here
+import ScrollUpButton from "react-scroll-up-button"; //Add this line
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Header extends React.Component {
     
@@ -12,84 +13,53 @@ class Header extends React.Component {
         }
     }
 
-    componentDidMount(){
-        // Get Companies All
-        axios.get(`${ServiceApi}/api/v1/company/1`).then( respone => {
-            this.setState({ companies: respone.data.data });
-        }).catch( error =>  console.log(error))
-    }
+    // componentDidMount(){
+    //     // Get Companies All
+    //     axios.get(`${ServiceApi}/api/v1/company/1`).then( respone => {
+    //         this.setState({ companies: respone.data.data });
+    //     }).catch( error =>  console.log(error))
+    // }
 
     render(){
         return(
-        <>
-            <div id="preloader"></div>
-            {/*============================== top-area-start================================*/}
-            <section className="top-area">
-                <div className="container">
-                <div className="row">
-                    <div className="top-area-content">
-                    {
-                        this.state.companies.map(item => {
-                            return <div className="col-md-6 col-sm-6 col-xs-5">
-                                <div className="logo text-left">
-                                    <a><img src={`${ServiceApi}/uploads/${item.logo}`} alt /></a>
-                                </div>
-                            </div>
-                        })
-                    }
-                    {/* <div className="col-md-6 col-sm-6 col-xs-7">
-                        <div className="social-icon text-right">
-                        <a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook" /></a>
-                        <a href="https://twitter.com/twitter" target="_blank" ><i className="fa fa-twitter" /></a>
-                        <a href="https://www.pinterest.com/"><i className="fa fa-pinterest" target="_blank" /></a>
-                        <a href="https://www.instagram.com/"><i className="fa fa-instagram" target="_blank" /></a>
-                        </div>
-                    </div> */}
+            <>
+                <div className="header-top">
+                    <div className="row">
+                         <div className="col-md-12">
+                             <div className="row">
+                                 <div className="col-md-6">
+                                    <div className="logo">
+                                        <img src="assets/img/logo/logo.jpg" />
+                                    </div>
+                                 </div>
+                                 <div className="col-md-6">
+                                    <div className="logo">
+                                        <ul className="menu-top">
+                                            <li className="active">
+                                                <a href="#">Home</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Our People</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Our Work</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Contact Us</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">About Us</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Publication</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                 </div>
+                             </div>
+                         </div>
                     </div>
                 </div>
-                </div>
-            </section>
-            {/*============================== menu-area-start ================================*/}
-            <section className="menu-area">
-                <div className="container">
-                <div className="row">
-                    <div className="menu-area-content clearfix">
-                    <div className="col-md-12 col-sm-9 col-xs-12">
-                        <div className="main-menu text-left">
-                        <div className="navbar-header">
-                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar" />
-                            <span className="icon-bar" />
-                            <span className="icon-bar" />
-                            </button>
-                        </div>
-                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul className="nav navbar-nav">                                       
-                            <li><a href="/index/home">HOME</a></li>
-                            <li><a href="/index/ourpeople">OUR PEOPLE</a></li>
-                            <li><a href="/index/ourwork">OUR WORK</a></li>
-                            <li><a href="/index/product">PRODUCT</a></li>
-                            <li><a href="/index/contact">CONTACT US</a></li>
-                            <li><a href="/index/about">ABOUT US</a></li>
-                            <li><a href="/index/publication">PUBLICATION</a></li>
-                            </ul>
-                        </div>    
-                        </div>
-                    </div>
-                    {/* <div className="col-md-5 col-sm-3 col-xs-12">
-                        <div className="search-btn text-right">
-                        <form>
-                            <input type="search" placeholder="Type to search here" />
-                            <button type="submit"><i className="fa fa-search" /></button>
-                        </form>
-                        </div>
-                    </div> */}
-                    </div>
-                </div>
-                </div>
-            </section>
-            <ScrollUpButton style={{width: 50}} ToggledStyle={{right: 20}}/>
             </>
         );
     }
--- dc-front/src/pages/home/Home.js
+++ dc-front/src/pages/home/Home.js
@@ -7,80 +7,8 @@ class Home extends React.Component {
         return (
             <>
             <Header />
-            {/*============================== categories-area-start ================================*/}
-            <section className="categories-area">
-                <div className="container">
-                <div className="row">
-                    <div className="col-md-4 col-sm-4">
-                    <div className="category-border-content">
-                            <div className="category-detail">
-                                <div className="category-img">
-                                    <img class="img-thumbnail" src="/assets/img/post/1.jpg" alt />
-                                    {/* <div className="category-overlay">
-                                    </div> */}
-                                </div>
-                            </div>
-                            <div className="category-detail">
-                                <div className="category-img">
-                                    <img class="img-thumbnail" src="/assets/img/post/2.jpg" alt />
-                                    {/* <div className="category-overlay">
-                                    </div> */}
-                                </div>
-                            </div>
-                            <div className="category-detail">
-                                <div className="category-img">
-                                    <img class="img-thumbnail" src="/assets/img/post/3.jpg" alt />
-                                    {/* <div className="category-overlay">
-                                    </div> */}
-                                </div>
-                            </div>
-                            <div className="category-detail">
-                                <div className="category-img">
-                                    <img class="img-thumbnail" src="/assets/img/post/4.jpg" alt />
-                                    {/* <div className="category-overlay">
-                                    </div> */}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-8 col-sm-8">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <span style={{fontSize: "25px", fontWeight: "bold", color: "gray"}}>WELCOME TO </span>
-                                <span style={{fontSize: "25px", fontWeight: "bold", color: "red"}}> CCA</span>
-                                <span style={{fontSize: "25px", fontWeight: "bold", color: "gray"}}> Cosmetics Of </span>
-                                <span style={{fontSize: "25px", fontWeight: "bold", color: "gray"}}> Consultant Association</span>
-
-                                <p style={{fontSize: "25px", fontWeight: "bold", marginBottom: "10px", position: "relative", left: "235px", lineHeight: "2"}}>ព្រះរាជាណាចក្រកម្ពុជា</p>
-                                <p style={{fontSize: "25px", fontWeight: "bold", marginBottom: "10px", position: "relative", left: "212px", lineHeight: "2"}}>ជាតិ សាសនា​ ព្រះមហាក្សត្រ</p>
-                                <p style={{position: "relative", left: "320px", fontSize: "18px", lineHeight: "2"}}>លក្ខន្តិកៈ</p>
-                                <p style={{position: "relative", left: "200px", fontSize: "18px", lineHeight: "2"}}>Cosmetics of Consultant Association</p>
-                                <p style={{position: "relative", left: "320px", fontSize: "18px", lineHeight: "2"}}>C.C.A</p>
-                                <p style={{position: "relative", left: "320px", fontSize: "18px", lineHeight: "2"}}>ជំពូកទី១</p>
-                                <p style={{position: "relative", left: "252px", fontSize: "18px", lineHeight: "2"}}>អំពីគោលបំណង និងបេសកម្ម</p>
-
-                                <p style={{fontSize: "25px", fontWeight: "bold", marginBottom: "10px", position: "relative", lineHeight: "2"}}>ប្រការ១៖ គោលបំណង</p>
-
-                                <p style={{position: "relative", left: "90px", fontSize: "15px", marginBottom: "10px"}}>សមាគមអ្នកប្រឹក្សាយោបលគ្រឿងសម្អាងមានគោលបំពណងដូចខាងក្រោម៖</p>
-                                <p style={{position: "relative", left: "90px", fontSize: "15px", marginBottom: "10px"}}>-ចូលរួមសហការជាមួយអជ្ញាធរ  ស្ថាប័នពាក់ព័ន្ធ លើកកម្ពស់ និងទប់ស្កាត់គ្រឿងសម្អាងក្លែងក្លាយដែលកំពុងបំផ្លាញសុខភាពអ្នកប្រើប្រាស។</p>
-                                <p style={{position: "relative", left: "90px", fontSize: "15px", marginBottom: "10px"}}>-គាំទ្រដល់ផលិតផល ដែលនាំចូលដោយមានការចុះបញ្ជីត្រឹមត្រូវ និងទទួលស្គាល់ដោយមន្ទីពាក់ព័ន្ធរបស់កម្ពុជា។</p>
-                                <p style={{position: "relative", left: "90px", fontSize: "15px", marginBottom: "10px"}}>-រកទីផ្សា កន្លែងតាំងពិព័រណ៌ និងបង្កើតទីផ្សារ សម្រាប់ដាក់តាំងលក់ផលិតផលគ្រឿងសម្អាងក្នុងស្រុក និងក្រៅស្រុក។</p>
-                                <p style={{position: "relative", left: "90px", fontSize: "15px", marginBottom: "10px"}}>-ជួយគាំពារ និងលើកកម្ពស់ដល់អាជីវករដែលជាម្ចាស់ផលិតផលគ្រឿងសម្អាងក្មុងស្រុកឲ្យមានការទទួលស្គាល់ជាផ្លូវការពីក្រស្ថាប័នពាក់ព័ន្ធ ដែលជាសក្ដានុពល ឆ្ពោះទៅរកការប្រកួតប្រជែងផលិតផលិតផលគ្រឿងសម្អាងកម្ពុជានៅលើទីផ្សា។</p>
-                                <p style={{position: "relative", left: "90px", fontSize: "15px", marginBottom: "10px"}}>-ជួយគាំពារ និងលើកកម្ពស់ដល់អាជីវករដែលជាម្ចាស់ផលិតផលគ្រឿងសម្អាងក្មុងស្រុកឲ្យមានការទទួលស្គាល់ជាផ្លូវការពីក្រស្ថាប័នពាក់ព័ន្ធ ដែលជាសក្ដានុពល ឆ្ពោះទៅរកការប្រកួតប្រជែងផលិតផលិតផលគ្រឿងសម្អាងកម្ពុជានៅលើទីផ្សា។</p>
-
-                                <p style={{fontSize: "25px", fontWeight: "bold", marginBottom: "10px", position: "relative", lineHeight: "2"}}>ប្រការ២៖ បេសកម្ម</p>
-
-                                <p style={{position: "relative", left: "90px", fontSize: "15px", marginBottom: "10px"}}>-គាំទ្រការខ្សែរសង្វាក់ផលិតផលិតផលគ្រឿងសម្អាងក្នុងស្រុក ដែលមានការទទួលស្គាល់ពូមន្ទីពិសោធន៍ជាតិ។ លើការផលិតតាមបច្ចេកទេសឲ្យបានត្រឹមត្រូវតាមស្ដាងដាក្រសួងពាក់ព័ន្ធ។</p>
-                                <p style={{position: "relative", left: "90px", fontSize: "15px", marginBottom: "10px"}}>-លើកស្ទួននូវផលិតផលក្នុងស្រុកចំពោះម្ចាស់កម្មសិទ្ធករ ដែលបានចុះបញ្ជីការទទួលស្គាល់គ្រប់ស្ថាប័នពាក់ព័ន្ធ ក្នុងការស្វែងរកទីផ្សារ ជៀសវាងការផលិត ឬវេចខ្ចប់ផលិតផលគ្រឿងសម្អាងក្លែងក្លាយ។</p>
-                                <p style={{position: "relative", left: "90px", fontSize: "15px", marginBottom: "10px"}}>-ផ្ដល់​ការប្រឹក្សាផ្លូវច្បាប់ ដោះស្រាយវិវាទដែលជាហានិភ័យរវាងម្ចាស់អាជីវកម្មគ្រឿងសម្អាងស្របច្បាប់នៅកម្ពុជា។</p>                             
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </section>
-            <Footer />
+            
+            {/* <Footer /> */}
             </>
         )
     }
